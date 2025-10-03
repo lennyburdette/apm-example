8bca08c253a1d56e4dfcb0c9c9993bb33583d4de
Lenny Burdette <lenny@apollographql.com>
2025-10-03 14:34:57 -0400

    new initial commit

--- 0test.sh
+++ 0test.sh
@@ -4,30 +4,28 @@ HERE=$(realpath "$(dirname "$0")")
 
 cd $HERE/packages/users
 apm install
-apm publish --yes --version 0.1.0
-apm tag dev --version 0.1.0
+apm publish --yes
+apm tag dev
 
 cd $HERE/packages/posts
 apm install
-apm publish --yes --version 0.1.0
-apm tag dev --version 0.1.0
+apm publish --yes
+apm tag dev
 
 cd $HERE/packages/supergraph
-# apm add @example/users#dev
-# apm add @example/posts#dev
-apm publish --yes --version 0.1.0
-apm tag dev --version 0.1.0
+apm publish --yes
+apm tag dev
 
 cd $HERE/packages/public
 apm install
-apm publish --yes --version 0.1.0
-apm tag dev --version 0.1.0
+apm publish --yes
+apm tag dev
 
 cd $HERE/packages/client-a
 apm install
-apm publish --yes --version 0.1.0
-apm tag dev --version 0.1.0
+apm publish --yes
+apm tag dev
 
-sed -i '' 's/# email/email/g' operations.graphql
-apm publish --yes --version 0.2.0
-apm tag dev --version 0.2.0
+# sed -i '' 's/# email/email/g' operations.graphql
+# apm publish --yes
+# apm tag dev
